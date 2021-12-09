6d3a2d608a9e16bcb3cc9352caadadc90e0de268
Grégoire Gavin <gregoiregavin@gmail.com>
2021-12-09 14:01:26 +0100

    test piotrowski

--- argentine-ornithology.py
+++ argentine-ornithology.py
@@ -1,15 +1,24 @@
 import re
+import fileinput
 
 with open ('files/1_birds.txt', 'r', encoding="utf8") as sourcefile :
     sourcetext = sourcefile.read()
 
-orders = re.findall('Order\s[IVXL]*\.\s[A-Z \Æ]*\.', sourcetext)
+# TEST 1
+# for order in (orders):
+#    families = re.findall('('+order+')*(Fam\.\s[IVXL]*\.\s[A-Z \Æ]*)', sourcetext)
+#    print("\n================================\n"+order+"\n"+"================================\n")
+#    for family in (families):
+#        print(family)
 
-for order in (orders):
-    families = re.findall('('+order+')*(Fam\.\s[IVXL]*\.\s[A-Z \Æ]*)', sourcetext)
-    print("\n================================\n"+order+"\n"+"================================\n")
-    for family in (families):
-        print(family)
+# TEST PIOTROWSKI   
+for order in fileinput.input(files=('files/1_birds.txt'), encoding='utf-8'):
+    if re.match('Order\s[IVXL]*\.\s[A-Z \Æ]*\.', order):
+        families = re.findall('(Fam\.\s[IVXL]*\.\s[A-Z \Æ]*)', sourcetext)
+        print("\n================================\n"+order+"\n"+"================================\n")
+        for family in (families):
+            print(family)
+        
 
 #Orders = re.findall('Order\s[IVXL]*\.\s[A-Z \Æ]*\.', sourcetext)
 #print(len(Orders)) # OK -> 18 orders
