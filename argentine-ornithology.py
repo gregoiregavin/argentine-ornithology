c3876f7d922673cc9bef6ee5d9c93e685dd0326a
Adriencoulonunil <95585176+Adriencoulonunil@users.noreply.github.com>
2021-12-12 15:36:51 +0100

    update Dimanche

--- argentine-ornithology.py
+++ argentine-ornithology.py
@@ -3,6 +3,44 @@ import fileinput
 
 with open ('files/1_birds.txt', 'r', encoding="utf8") as sourcefile :
     sourcetext = sourcefile.read()
+import xml.etree.ElementTree as ET
+
+# Create the XML tree that we're going to populate
+tree = ET.ElementTree(element = ET.Element('document'))
+root = tree.getroot()
+
+for line in fileinput.input(encoding='utf-8'):
+    m = re.match(r'^Order\s([IVXL]+)\.', line)
+    if m:
+        print(line)
+        order = ET.SubElement(root, 'order', attrib = {'n': m.group(1)})
+
+    m = re.match(r'^Fam\.\s([IVXL]+)\. (.+)$', line)
+    if m:
+        print(line)
+        family = ET.SubElement(order, 'family', attrib = {'n': m.group(1)})
+        family.text = m.group(2)
+        
+    m = re.match(r'(?m)^([0-9]{3}\. .*)$', line)
+    if m:
+        print(line)
+        family = ET.SubElement(order, 'nom', attrib = {'n': m.group(1)})
+        family.text = m.group(3)
+        
+    m = re.match(r'(?m)^(_Hab\._ .*)$', line)
+    if m:
+        print(line)
+        family = ET.SubElement(order, 'habitat', attrib = {'n': m.group(1)})
+        family.text = m.group(4)
+
+# Make the output readable
+ET.indent(tree)
+
+# Write the XML tree to a file
+tree.write('output.xml', encoding = 'utf-8')
+
+#hello
+
 
 # TEST 1
 # for order in (orders):
@@ -12,12 +50,12 @@ with open ('files/1_birds.txt', 'r', encoding="utf8") as sourcefile :
 #        print(family)
 
 # TEST PIOTROWSKI   
-for order in fileinput.input(files=('files/1_birds.txt'), encoding='utf-8'):
-    if re.match('Order\s[IVXL]*\.\s[A-Z \Æ]*\.', order):
-        families = re.findall('(Fam\.\s[IVXL]*\.\s[A-Z \Æ]*)', sourcetext)
-        print("\n================================\n"+order+"\n"+"================================\n")
-        for family in (families):
-            print(family)
+#for order in fileinput.input(files=('files/1_birds.txt'), encoding='utf-8'):
+   # if re.match('Order\s[IVXL]*\.\s[A-Z \Æ]*\.', order):
+        #families = re.findall('(Fam\.\s[IVXL]*\.\s[A-Z \Æ]*)', sourcetext)
+       # print("\n================================\n"+order+"\n"+"================================\n")
+       # for family in (families):
+           # print(family)
 
         
 
@@ -44,3 +82,4 @@ for order in fileinput.input(files=('files/1_birds.txt'), encoding='utf-8'):
 ## It's useful to store it in a list
 
 # Order((.|\n)*)
+
