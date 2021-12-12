533f11400b7387c162ecb47a27d6c5f7ae8f63c5
Grégoire Gavin <gregoiregavin@gmail.com>
2021-12-13 00:02:01 +0100

    indent

--- argentine-ornithology.py
+++ argentine-ornithology.py
@@ -2,7 +2,7 @@ import os
 import re
 import fileinput
 import xml.etree.ElementTree as ET
-#from utils.utils import checkResults
+from utils.utils import checkResults
 
 # Vars for regex results check
 #countOrders = 0
